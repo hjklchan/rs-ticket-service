36e4b3071e2d5365c3958d74fd4d4b3e07ad81d9
Lucas chen <lucas.chen@gbo-asia.com>
2024-05-30 14:58:53 +0800

    add migration file

--- migrations/20240530052750_create_tickets_table.up.sql
+++ migrations/20240530052750_create_tickets_table.up.sql
@@ -1,3 +1,11 @@
 -- Add up migration script here
 CREATE TABLE IF NOT EXISTS `tickets` (
+    `id` VARCHAR(50) NOT NULL,
+    `assignee_id` VARCHAR(50) NOT NULL,
+    `title` VARCHAR(100) NOT NULL,
+    `description` VARCHAR(255) NULL,
+    `body` TEXT NULL,
+    `status` TINYINT(1) NOT NULL,
+    `created_at` TIMESTAMP NULL,
+    `updated_at` TIMESTAMP NULL
 );
