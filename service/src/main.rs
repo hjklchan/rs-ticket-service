e6c1d3283b56ed324391206f6d22fcab0544b543
Illusion <1161524405@qq.com>
2024-05-30 20:06:37 +0800

    socket addr to make server

--- service/src/main.rs
+++ service/src/main.rs
@@ -1,6 +1,9 @@
+use std::net::SocketAddr;
+
 use abi::ticket_server::TicketServer;
 
 mod db;
+mod model;
 mod server;
 
 #[tokio::main]
@@ -8,11 +11,12 @@ async fn main() {
     let pool = db::connect().await;
 
     let service = server::TicketService::new(pool);
+
+    let socket_addr = SocketAddr::from(([127, 0, 0, 1], 50061));
+
     tonic::transport::Server::builder()
         .add_service(TicketServer::new(service))
-        .serve("0.0.0.0:50061".parse().unwrap())
+        .serve(socket_addr)
         .await
         .unwrap();
-
-    println!("Hello, world!");
 }
